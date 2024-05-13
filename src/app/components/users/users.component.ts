78c235776553f5b4e157abd8035bd6b2dd371e0a
Prosper Nnadi <prospernnadi3@gmail.com>
2024-05-13 22:31:30 +0100

    Completed Pagination

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Response } from '../../interfaces/user.interface';
-import { tap } from 'rxjs';
 import { NgForOf, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -10,16 +9,33 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [NgForOf, NgIf, RouterLink],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  styleUrl: './users.component.scss',
 })
 export class UsersComponent implements OnInit {
-  userResp!: Response
-  private readonly userServ = inject(UserService)
+  userResp!: Response;
+  private readonly userServ = inject(UserService);
 
-ngOnInit(): void {
-  this.userServ.fetchAllUsers(10).subscribe((res: Response) => {
-    console.log('Res', res);
-    this.userResp = res;
-  });
-}
+  ngOnInit(): void {
+    this.userServ.fetchAllUsers(24).subscribe((res: Response) => {
+      this.userResp = res;
+    });
+  }
+
+  currentPage = 1;
+  itemsPerPage = 3;
+
+  onPageChange(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
+
+  getTotalPages() {
+    const totalPages = Math.ceil(this.userResp?.results.length / this.itemsPerPage);
+    return Array(totalPages).fill(0).map((_, index) => index + 1);
+  }
+
+  getCurrentPageData() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.userResp?.results.slice(startIndex, endIndex);
+  }
 }
